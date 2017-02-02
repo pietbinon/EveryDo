99c15f0ac12d4de926c1996cc178201c75024bd1
Jonester <chris.jones78@gmail.com>
2017-02-01 20:14:07 -0500

    Committing before branching.

--- EveryDo/AddToDoViewController.m
+++ EveryDo/AddToDoViewController.m
@@ -32,18 +32,7 @@
     ToDo *toDo = [[ToDo alloc]initWithTitle:self.addTitle.text todoDescription:self.addDescription.text priority:[self.addPriority.text integerValue] isComplete:NO];
     
     [self.delegate addToDoObject:toDo];
-
     [self dismissViewControllerAnimated:YES completion:nil];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- EveryDo/MasterViewController.m
+++ EveryDo/MasterViewController.m
@@ -93,14 +93,14 @@
 }
 
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [self.toDos removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-}
+//- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+//    if (editingStyle == UITableViewCellEditingStyleDelete) {
+//        [self.toDos removeObjectAtIndex:indexPath.row];
+//        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+//    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+//        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+//    }
+//}
 
 - (void)addToDoObject:(ToDo *)toDo {
     [self.toDos insertObject:toDo atIndex:0];
